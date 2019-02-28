195c6b7d4e9962c01407cea8903253fbc59c7f18
yy <1023767856@qq.com>
2019-02-28 15:22:43 +0800

    ok

---
+++ config/dbconfig.py
@@ -0,0 +1,11 @@
+
+def dbconfig():
+    config = {
+        "username": "root",
+        "password": "root",
+        "host": "127.0.0.1",
+        "port": "3306",
+        "database": "database",
+        "table_prefix": "",
+    }
+    return config
