554f0bb7d9ac00c660183131011d5e5c0caa6e60
yy <1023767856@qq.com>
2019-07-25 16:01:33 +0800

    ready to upload to pip

---
+++ tool_yy/config/dbconfig.py
@@ -0,0 +1,11 @@
+
+def dbconfig():
+    config = {
+        "username": "",
+        "password": "",
+        "host": "127.0.0.1",
+        "port": "3306",
+        "database": "ex",
+        "table_prefix": "ex_",
+    }
+    return config
---
+++ tool_yy/config/phoenix_db_config.py
@@ -0,0 +1,7 @@
+def dbconfig():
+    config = {
+        "host": "127.0.0.1:8765",
+        "table_prefix": "law_",
+        "debug": True
+    }
+    return config
